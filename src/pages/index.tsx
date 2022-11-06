461aca97974970d7aa2b9a6132d4234417dda1e0
Manish Shrestha <maniz.stha@gmail.com>
2022-11-06 12:58:17 +0545

    Create a simple coming soon page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,33 @@
 import * as React from "react"
 import type { HeadFC } from "gatsby"
 
+const pageStyles = {
+  display: 'flex',
+  height: '100vh',
+  justifyContent: 'space-around',
+  alignItems: 'center'
+}
+
+const infoStyles = {
+  fontSize: '20px'
+}
+
+const comingSoonStyles = {
+  fontSize: '50px'
+}
+
 const IndexPage = () => {
   return (
-    <main>
-      <h1>
-        Welcome
-      </h1>
-      <p>To the personal site of Manish Shrestha</p>
-      <p>More info</p>>
+    <main style={pageStyles}>
+      <div style={infoStyles}>
+        <h1>
+          Manish Shrestha
+        </h1>
+        <p>Programmer, Software Engineer, Full Stack Developer</p>
+      </div>
+      <div style={comingSoonStyles}>
+        <h2>Coming SOON</h2>
+      </div>
     </main>
   )
 }
