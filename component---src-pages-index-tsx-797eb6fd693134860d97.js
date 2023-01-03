2ee5c7937b293be6fb84c6a92ee0a1730cffd968
Manish Shrestha <maniz.stha@gmail.com>
2023-01-03 22:51:47 +0545

    Updates

--- component---src-pages-index-tsx-797eb6fd693134860d97.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmaniz_stha=self.webpackChunkmaniz_stha||[]).push([[691],{7200:function(e,n,t){t.r(n),t.d(n,{Head:function(){return l}});var a=t(7294);n.default=function(){return a.createElement("main",null,a.createElement("h1",null,"Welcome"),a.createElement("p",null,"To the personal site of Manish Shrestha"),a.createElement("p",null,"More info"),">")};var l=function(){return a.createElement("title",null,"Manish Shrestha: Personal page")}}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-797eb6fd693134860d97.js.map
