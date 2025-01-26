9914aeac23a1f771a394589c566ff713d925afbf
Manish Shrestha <maniz.stha@gmail.com>
2025-01-26 17:13:40 +0545

    Update info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
 import type { HeadFC } from "gatsby"
+import downloadFile from '../files/manish_resume.pdf'
 
 const pageStyles = {
   display: 'flex',
+  flexWrap: 'wrap',
   height: '100vh',
   justifyContent: 'space-around',
   alignItems: 'center'
@@ -16,6 +18,10 @@ const comingSoonStyles = {
   fontSize: '50px'
 }
 
+const lazyStyles = {
+  fontSize: '20px'
+}
+
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
@@ -25,8 +31,14 @@ const IndexPage = () => {
         </h1>
         <p>Programmer, Software Engineer, Full Stack Developer</p>
       </div>
-      <div style={comingSoonStyles}>
-        <h2>Coming SOON</h2>
+      <div>
+        <div style={comingSoonStyles}>
+          <h2>Coming SOON</h2>
+        </div>
+        <div style={lazyStyles}>
+          <i>Read: Too lazy and lacking creative ideas on how to start on an actual portfolio site.</i>
+          <p>Meanwhile go ahead and download my cv that contains some info about me <a href={downloadFile}>here</a></p>
+        </div>
       </div>
     </main>
   )
